85f7c5baf3f021349aef6fa97614810d7b170d7b
RONIT CHANDRESH VIRWANI <ronitvirwani1@gmail.com>
2024-02-01 19:16:51 +0530

    Update login-form.tsx

--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -3,26 +3,47 @@
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/lib/supabase/supabase-auth-provider";
 import { useRouter } from "next/navigation";
-import { Github, Laptop } from "lucide-react";
-import { useEffect } from "react";
+import { Github, Laptop, Mail } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/seperator";
+import { useEffect, useState } from "react";
 
 const LoginForm = () => {
-  const { signInWithGithub, user } = useAuth();
-  const { signInWithGoogle} = useAuth();
+  const { signInWithEmail, signUpWithEmail, signInWithGithub, user } = useAuth();
+  const { signInWithGoogle } = useAuth();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [isSignUp, setIsSignUp] = useState(false); // Add state to track sign-up mode
   const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null);
+    try {
+      if (isSignUp) {
+        await signUpWithEmail(email, password);
+      } else {
+        await signInWithEmail(email, password);
+      }
+    } catch (error) {
+      setError("Something went wrong!");
+      console.error("Error:", error);
+    }
+  };
 
   // Check if there is a user
   useEffect(() => {
     if (user) {
       router.push("/");
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
   return (
     <div className="flex items-center w-full h-full px-8">
-      {/* Main Container */}
-      <div className="w-full ">
-        {/* Text */}
+      <div className="w-full">
         <div>
           <h1 className="text-4xl font-bold">Login</h1>
           <p className="mt-2 text-neutral-600 dark:text-neutral-400">
@@ -33,20 +54,61 @@ const LoginForm = () => {
             Please login with your Google account.
           </p>
         </div>
-        {/* Github Button */}
         <Button
           onClick={signInWithGithub}
-          className="flex items-center w-full gap-2 mt-6"
+          className="flex items-center w-half gap-2 mt-6"
         >
-          Login with your Github <Github size="16"/>
+          Login with your Github <Github size="16" />
         </Button>
-        {/* Google Button */}
         <Button
           onClick={signInWithGoogle}
-          className="flex items-center w-full gap-2 mt-6"
+          className="flex items-center w-half gap-2 mt-6"
         >
-          Login with Google <Laptop size="16"/>
+          Login with Google <Laptop size="16" />
         </Button>
+        <div className="flex items-center my-8">
+          <Separator /> <span className="mx-6">OR</span> <Separator />
+        </div>
+        <div className="flex items-center my-8">
+          <form onSubmit={handleSubmit}>
+            <div className="mt-6 space-y-6">
+              <div className="space-y-2">
+                <Label>Email</Label>
+                <Input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="space-y-2">
+                <Label>Password</Label>
+                <Input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+            {error && <div className="mt-4 text-red-500">{error}</div>}
+            <Button
+              variant="subtle"
+              type="submit"
+              className="flex items-center w-full gap-2 mt-6"
+            >
+              {isSignUp ? "Sign Up with Email" : "Login with Email"}
+              <Mail size="16" />
+            </Button>
+          </form>
+          <p className="mt-4 text-sm text-neutral-600 dark:text-neutral-400">
+            {isSignUp
+              ? "Already have an account?"
+              : "Don't have an account yet?"}{" "}
+            <span
+              className="text-primary-500 cursor-pointer"
+              onClick={() => setIsSignUp(!isSignUp)}
+            >
+              {isSignUp ? "Login" : "Sign Up"}
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
